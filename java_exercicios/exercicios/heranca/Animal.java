c4fc48350ceb20e9cf48d86084863c34d06060bb
veronica pupo <veronica.pupo@hotmail.com>
2022-08-16 18:27:44 -0300

    novos exercicios

--- java_exercicios/exercicios/heranca/Animal.java
+++ java_exercicios/exercicios/heranca/Animal.java
@@ -57,4 +57,8 @@ public abstract class Animal {
 comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as
 características de forma que tudo o que for comum a todos os animais fique na classe
 Animal:
+
+2-Implemente um programa que crie os 3 tipos de animais definidos no exercício
+anterior e invoque o método que emite o som de cada um de forma polimórfica, isto
+é, independente do tipo de animal.
  */
